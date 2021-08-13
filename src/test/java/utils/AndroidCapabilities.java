4676067d7fa693848000ed9ee5ea98eec102bd6f
Tassio.Lima <tassio.lima@tui.co.uk>
2021-08-13 11:52:26 -0300

    Parallel test test

--- src/test/java/utils/AndroidCapabilities.java
+++ src/test/java/utils/AndroidCapabilities.java
@@ -8,18 +8,20 @@ public class AndroidCapabilities {
 
     public static DesiredCapabilities getAndroidCapabilities(){
         DesiredCapabilities caps = new DesiredCapabilities();
-      //  caps.setCapability("device", Utils.pathCapabilitie().getDevices()[0]);
-     //   caps.setCapability("os_version", Utils.pathCapabilitie().getOSVersions()[0]);
+        caps.setCapability("device", Utils.pathCapabilitie().getDevices()[0]);
+        caps.setCapability("os_version", Utils.pathCapabilitie().getOSVersions()[0]);
         caps.setCapability("project", "Android Project");
         caps.setCapability("build", "Automation");
         caps.setCapability("name", "Android");
         caps.setCapability("app", Utils.pathCapabilitie().getApps()[0]);
         caps.setCapability("browserstack.video", false);
         caps.setCapability("browserstack.debug", true);
-        caps.setCapability("environments", Utils.pathCapabilitie().getEnvironment()[0].getDevices());
+        System.out.println(Utils.pathCapabilitie().getEnvironment()[0].getDevices() +"  " + "  "+ Utils.pathCapabilitie().getEnvironment()[0].getOSVersions() );
+      /*  caps.setCapability("environments", Utils.pathCapabilitie().getEnvironment()[0].getDevices());
         caps.setCapability("environments", Utils.pathCapabilitie().getEnvironment()[0].getOSVersions());
         caps.setCapability("environments", Utils.pathCapabilitie().getEnvironment()[1].getDevices());
         caps.setCapability("environments", Utils.pathCapabilitie().getEnvironment()[1].getOSVersions());
+        */
         return caps;
     }
 
